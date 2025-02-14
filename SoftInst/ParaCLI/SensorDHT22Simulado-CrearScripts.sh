3fbbc4e5bddd390a485928ffa653f4f91e1e5df9
NiPeGun <nipegun@gmail.com>
2025-02-14 10:01:11 +0100

    Update SensorDHT22Simulado-CrearScripts.sh

--- SoftInst/ParaCLI/SensorDHT22Simulado-CrearScripts.sh
+++ SoftInst/ParaCLI/SensorDHT22Simulado-CrearScripts.sh
@@ -8,63 +8,71 @@
 # ----------
 # Script de NiPeGun para crear los scripts de simulación de lectura del sensor DHT22 para Debian
 #
-# Ejecución remota:
-#  curl -sL https://raw.githubusercontent.com/nipegun/ot-scripts/refs/heads/main/SoftInst/ParaCLI/SensorDHT22Simulado-CrearScripts.sh | bash
+# Ejecución remota (puede requerir permisos sudo):
+#   curl -sL https://raw.githubusercontent.com/nipegun/ot-scripts/refs/heads/main/SoftInst/ParaCLI/SensorDHT22Simulado-CrearScripts.sh | bash
+#
+# Ejecución remota como root (para sistemas sin sudo):
+#   curl -sL https://raw.githubusercontent.com/nipegun/ot-scripts/refs/heads/main/SoftInst/ParaCLI/SensorDHT22Simulado-CrearScripts.sh | sed 's-sudo--g' | bash
 # ----------
 
 # Creación del script de simulación de lectura del sensor
-   echo '#!/usr/bin/python3'                                                                          > /root/scripts/SensorDHT22Simulado-Leer.py
-   echo ''                                                                                           >> /root/scripts/SensorDHT22Simulado-Leer.py
-   echo 'from time import time, sleep'                                                               >> /root/scripts/SensorDHT22Simulado-Leer.py
-   echo 'import json'                                                                                >> /root/scripts/SensorDHT22Simulado-Leer.py
-   echo 'from random import uniform'                                                                 >> /root/scripts/SensorDHT22Simulado-Leer.py
-   echo ''                                                                                           >> /root/scripts/SensorDHT22Simulado-Leer.py
-   echo 'while True:'                                                                                >> /root/scripts/SensorDHT22Simulado-Leer.py
-   echo '  sleep(1 - time() % 1)'                                                                    >> /root/scripts/SensorDHT22Simulado-Leer.py
-   echo "  print(json.dumps({'Temperatura': uniform(10, 45), 'Humedad': uniform(11,99)}, indent=2))" >> /root/scripts/SensorDHT22Simulado-Leer.py
-   chmod +x /root/scripts/SensorDHT22Simulado-Leer.py
+  echo '#!/usr/bin/python3'                                                                         | sudo tee    /root/scripts/SensorDHT22Simulado-Leer.py
+  echo ''                                                                                           | sudo tee -a /root/scripts/SensorDHT22Simulado-Leer.py
+  echo 'from time import time, sleep'                                                               | sudo tee -a /root/scripts/SensorDHT22Simulado-Leer.py
+  echo 'import json'                                                                                | sudo tee -a /root/scripts/SensorDHT22Simulado-Leer.py
+  echo 'from random import uniform'                                                                 | sudo tee -a /root/scripts/SensorDHT22Simulado-Leer.py
+  echo ''                                                                                           | sudo tee -a /root/scripts/SensorDHT22Simulado-Leer.py
+  echo 'while True:'                                                                                | sudo tee -a /root/scripts/SensorDHT22Simulado-Leer.py
+  echo '  sleep(1 - time() % 1)'                                                                    | sudo tee -a /root/scripts/SensorDHT22Simulado-Leer.py
+  echo "  print(json.dumps({'Temperatura': uniform(10, 45), 'Humedad': uniform(11,99)}, indent=2))" | sudo tee -a /root/scripts/SensorDHT22Simulado-Leer.py
+  sudo chmod +x /root/scripts/SensorDHT22Simulado-Leer.py
 
 # Creación del script para el servicio
-   echo '#!/usr/bin/python3'                                                                                                 > /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
-   echo ''                                                                                                                  >> /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
-   echo 'import json'                                                                                                       >> /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
-   echo 'from random import uniform'                                                                                        >> /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
-   echo 'import datetime'                                                                                                   >> /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
-   echo 'from influxdb import client as influxdb'                                                                           >> /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
-   echo ''                                                                                                                  >> /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
-   echo '# Variables'                                                                                                       >> /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
-   echo "influxHost = 'xxx'"                                                                                                >> /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
-   echo "influxPort = 'xxx'"                                                                                                >> /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
-   echo "influxDB = 'xxx'"                                                                                                  >> /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
-   echo "influxUser = 'xxx'"                                                                                                >> /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
-   echo "influxPasswd = 'xxx'"                                                                                              >> /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
-   echo ''                                                                                                                  >> /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
-   echo '# Simular el objeto del sensor'                                                                                    >> /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
-   echo 'humidity, temperature = uniform(10, 45), uniform(10, 45)'                                                          >> /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
-   echo ''                                                                                                                  >> /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
-   echo '# Adaptar el formato de la hora para que influxDB lo entienda (2017-02-26T13:33:49.00279827Z)'                     >> /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
-   echo "current_time = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')"                                          >> /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
-   echo ''                                                                                                                  >> /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
-   echo 'influx_metric = [{'                                                                                                >> /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
-   echo '"measurement": "temp_hume",'                                                                                       >> /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
-   echo '               "time": current_time,'                                                                              >> /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
-   echo '               "fields":'                                                                                          >> /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
-   echo '                 {'                                                                                                >> /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
-   echo '                   "Temperatura": temperature,'                                                                    >> /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
-   echo '                   "Humedad": humidity'                                                                            >> /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
-   echo '                 }'                                                                                                >> /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
-   echo '}]'                                                                                                                >> /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
-   echo ''                                                                                                                  >> /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
-   echo '# Salvar mediciones a la base de datos'                                                                            >> /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
-   echo 'try:'                                                                                                              >> /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
-   echo '  db = influxdb.InfluxDBClient(influxHost, influxPort, influxUser, influxPasswd, influxDB)'                        >> /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
-   echo '  db.write_points(influx_metric)'                                                                                  >> /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
-   echo 'finally:'                                                                                                          >> /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
-   echo '  db.close()'                                                                                                      >> /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
-   #echo '# Imprimir también a un archivo de log'                                                                            >> /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
-   #echo 'with open('/var/log/dht22.log', 'a') as archivolog:'                                                               >> /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
-   #echo '  print("Se han guardado los siguientes valores: Humedad:",humidity, "Temperatura:",temperature, file=archivolog)' >> /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
-   #chmod +x /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
-   #touch /var/log/dht22.log
-   #chmod 777 /var/log/dht22.log
+   echo '#!/usr/bin/python3'                                                                                                | sudo tee    /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
+   echo ''                                                                                                                  | sudo tee -a /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
+   echo 'import json'                                                                                                       | sudo tee -a /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
+   echo 'from random import uniform'                                                                                        | sudo tee -a /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
+   echo 'import datetime'                                                                                                   | sudo tee -a /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
+   echo 'from influxdb import client as influxdb'                                                                           | sudo tee -a /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
+   echo ''                                                                                                                  | sudo tee -a /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
+   echo '# Variables'                                                                                                       | sudo tee -a /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
+   echo "influxHost = 'xxx'"                                                                                                | sudo tee -a /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
+   echo "influxPort = 'xxx'"                                                                                                | sudo tee -a /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
+   echo "influxDB = 'xxx'"                                                                                                  | sudo tee -a /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
+   echo "influxUser = 'xxx'"                                                                                                | sudo tee -a /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
+   echo "influxPasswd = 'xxx'"                                                                                              | sudo tee -a /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
+   echo ''                                                                                                                  | sudo tee -a /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
+   echo '# Simular el objeto del sensor'                                                                                    | sudo tee -a /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
+   echo 'humidity, temperature = uniform(10, 45), uniform(10, 45)'                                                          | sudo tee -a /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
+   echo ''                                                                                                                  | sudo tee -a /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
+   echo '# Adaptar el formato de la hora para que influxDB lo entienda (2017-02-26T13:33:49.00279827Z)'                     | sudo tee -a /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
+   echo "current_time = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')"                                          | sudo tee -a /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
+   echo ''                                                                                                                  | sudo tee -a /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
+   echo 'influx_metric = [{'                                                                                                | sudo tee -a /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
+   echo '"measurement": "temp_hume",'                                                                                       | sudo tee -a /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
+   echo '               "time": current_time,'                                                                              | sudo tee -a /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
+   echo '               "fields":'                                                                                          | sudo tee -a /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
+   echo '                 {'                                                                                                | sudo tee -a /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
+   echo '                   "Temperatura": temperature,'                                                                    | sudo tee -a /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
+   echo '                   "Humedad": humidity'                                                                            | sudo tee -a /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
+   echo '                 }'                                                                                                | sudo tee -a /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
+   echo '}]'                                                                                                                | sudo tee -a /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
+   echo ''                                                                                                                  | sudo tee -a /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
+   echo '# Salvar mediciones a la base de datos'                                                                            | sudo tee -a /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
+   echo 'try:'                                                                                                              | sudo tee -a /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
+   echo '  db = influxdb.InfluxDBClient(influxHost, influxPort, influxUser, influxPasswd, influxDB)'                        | sudo tee -a /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
+   echo '  db.write_points(influx_metric)'                                                                                  | sudo tee -a /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
+   echo 'finally:'                                                                                                          | sudo tee -a /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
+   echo '  db.close()'                                                                                                      | sudo tee -a /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
+   echo '# Imprimir también a un archivo de log'                                                                            | sudo tee -a /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
+   echo 'with open('/var/log/dht22.log', 'a') as archivolog:'                                                               | sudo tee -a /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
+   echo '  print("Se han guardado los siguientes valores: Humedad:",humidity, "Temperatura:",temperature, file=archivolog)' | sudo tee -a /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
+   sudo chmod +x /root/scripts/SensorDHT22Simulado-LeerYGuardarEnInfluxDB.py
+   sudo touch /var/log/dht22.log
+   sudo chmod 777 /var/log/dht22.log
 
+# Notificar fin de instalación del script
+  echo ""
+  echo "  Script finalizado."
+  echo "  Recuerda que la base de datos InfluxDB debe estar instalada para que el script funciona correctamente."
+  echo ""
