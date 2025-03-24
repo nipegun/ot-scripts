c98b62bdad9bfbc75d0ed58b50a68cf26c15aab1
NiPeGun <nipegun@gmail.com>
2025-03-24 06:24:11 +0100

    Update RevertirPayloadConAntiReplay.py

--- Simuladores/PLCs/Siemens-S7-1200/RevertirPayloadConAntiReplay.py
+++ Simuladores/PLCs/Siemens-S7-1200/RevertirPayloadConAntiReplay.py
@@ -1,13 +1,13 @@
+#!/usr/bin/env python3
+
+#
+# Revierte una cadena/payload con el anti-replay inyectado a su estado original antes de la inyección deduciendo el valor original del challenge directamente del string.
+#
+
+import sys
+
 def fRevertirAntiReplay(pRespChallenge, pPosHex=46):
-  """ Revierte la inserción del valor anti-replay en una cadena hexadecimal, deduciendo el valor original del challenge directamente del string.
 
-  Parámetros:
-    pRespChallenge (str): cadena hexadecimal con anti-replay inyectado
-    pPosHex (int): posición del primer carácter sobrescrito (por defecto 46)
-
-  Devuelve:
-    str: cadena hexadecimal con el byte modificado restaurado
-  """
   if len(pRespChallenge) < pPosHex + 4:
     raise ValueError("La cadena es demasiado corta para contener los campos necesarios.")
 
@@ -30,7 +30,19 @@ def fRevertirAntiReplay(pRespChallenge, pPosHex=46):
 
   return vRespRestaurado
 
+def main():
+  if len(sys.argv) != 2:
+    print("Uso: python3 revertir_antireplay.py <cadena_hexadecimal>")
+    sys.exit(1)
+
+  vCadenaConAntiReplayInyectado = sys.argv[1].lower()
+
+  try:
+    vCadenaOriginal = fRevertirAntiReplay(vCadenaConAntiReplayInyectado)
+    print(vCadenaOriginal)
+  except Exception as e:
+    print(f"Error: {e}")
+    sys.exit(1)
+
 if __name__ == "__main__":
-  vCadenaConAntiReplayInyectado = "0300004302f0807202003431000004f200000010000003a43400000034019077000803000004e88969001200000000896a001300896b00040000000000000072020000"
-  vCadenaOriginal = fRevertirAntiReplay(vCadenaConAntiReplayInyectado)
-  print(vCadenaOriginal)
+  main()
