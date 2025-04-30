ed31d9660e140decda65b799c48d48779f7c2f12
NiPeGun <nipegun@gmail.com>
2025-04-30 09:57:48 +0200

    Update PrepararEnProxmox.sh

--- EntornosVulnerables/GRFICSv2/PrepararEnProxmox.sh
+++ EntornosVulnerables/GRFICSv2/PrepararEnProxmox.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#!/bin/bash
-
 # Pongo a disposición pública este script bajo el término de "software de dominio público".
 # Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
